c33aa0adcc289bfc15bb4c0d27478bfba407ee08
nishisuke <nishisuke13+git@gmail.com>
2018-02-28 10:49:29 +0900

    refact

--- lib/ab_switcher/switcher.rb
+++ lib/ab_switcher/switcher.rb
@@ -2,21 +2,42 @@ module ABSwitcher
   class Switcher
     HEX_MAX = 15.0
 
-    attr_reader :major_probability
+    attr_reader :sorted_thresholds
 
-    def initialize(ratio)
-      @major_probability = calc_major_probability(ratio)
+    def initialize(ratios)
+      if ratios.is_a?(Numeric)
+        @sorted_thresholds = calc_major_probability(ratios)
+      elsif ratios.is_a?(Array) && ratios.size == 1
+        @sorted_thresholds = calc_major_probability(ratios[0])
+      elsif ratios.is_a?(Array)
+        @sorted_thresholds = calc_probabilities_from_ratios(ratios)
+      else
+        raise ArgumentError
+      end
     end
 
     def major_hex?(hex_str)
-      hex_probability(hex_str) <= major_probability
+      hex_switch(hex_str).zero?
+    end
+
+    def hex_switch(hex_str)
+      res = 0
+      sorted_thresholds.each.with_index do |threshold, i|
+        if hex_probability(hex_str) <= threshold
+          res = i
+          break
+        end
+      end
+
+      res
     end
 
     private
 
     def calc_major_probability(ratio)
       probability = convert_ratio_into_probability(ratio.to_f)
-      probability > 0.5 ? probability : 1 - probability
+      major = probability > 0.5 ? probability : 1 - probability
+      return major, 1
     end
 
     def convert_ratio_into_probability(ratio)
