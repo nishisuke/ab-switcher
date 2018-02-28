e3b1e304bdff8cb03a3cefca184ac7811a3c032d
nishisuke <nishisuke13+git@gmail.com>
2018-02-28 15:22:31 +0900

    api

--- lib/ab_switcher.rb
+++ lib/ab_switcher.rb
@@ -8,5 +8,9 @@ module ABSwitcher
     def major_hex?(hex_str, *ratios)
       ABSwitcher::Switcher.new(ratios).major_hex?(hex_str)
     end
+
+    def hex_switch(hex_str, *ratios)
+      ABSwitcher::Switcher.new(ratios).hex_switch(hex_str)
+    end
   end
 end
--- test/new_api_test.rb
+++ test/new_api_test.rb
@@ -2,7 +2,6 @@ require_relative 'test_helper'
 
 class NewApiTest < Minitest::Test
   def test_initialize_with_array
-    switcher = ABSwitcher::Switcher.new([1, 2, 3])
-    assert_equal 1, switcher.hex_switch('ab41')
+    assert_equal 1, ABSwitcher.hex_switch('ab41', 1 , 2, 3)
   end
 end
